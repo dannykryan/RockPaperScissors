d50a03870c1319d7d359a67bca5c3914ccb36f1e
dannykryan <dannykryan@gmail.com>
2022-10-03 07:12:46 +0100

    Improve UI 	Add player emoji 	Add score counter 	Add window boxes 	Add styling

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let list = ["Rock", "Paper", "Scissors"];
 
+// this function generates a random number between 0-2 to select R/P/S from 'list' //
 function getComputerChoice () {
 	return list[Math.floor((Math.random()*list.length))];
   }
 
+// this function compares the player selection to the computer selection and declares the player a winner or loser //
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
 	alert("You Win! Rock beats Scissors.");
@@ -28,8 +30,38 @@ function playRound(playerSelection, computerSelection) {
 }
 }
 
+// loops the game round //
 for (let i = 0; i < 1; i++) {
 const playerSelection = prompt("Rock, Paper or Scissors", "Make a Choice");
 const computerSelection = getComputerChoice();
 playRound(playerSelection, computerSelection);
 }
+
+// target buttons DOM nodes //
+// const rockButton = document.querySelector('.rock');
+// const paperButton = document.querySelector('.paper');
+// const scissorsButton = document.querySelector('.scissors');
+
+// const rockBtn = document.querySelector('.rock');
+// rockBtn.onclick = () => playerSelection = "rock";
+
+// const paperBtn = document.querySelector('.paper');
+// paperBtn.onclick = () => playerSelection = "paper";
+
+// const scissorsBtn = document.querySelector('.scissors');
+// scissorsBtn.onclick = () => playerSelection = "scissors";
+
+const rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click', () => {
+  playerSelection("rock");
+});
+
+const paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click', () => {
+  playerSelection("paper");
+});
+
+const scissorsBtn = document.querySelector('.scissors');
+scissorsBtn.addEventListener('click', () => {
+  console.log("scissors");
+});
