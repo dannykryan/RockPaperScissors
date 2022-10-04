ba6f332a7b573124b942143323179bb269357174
dannykryan <dannykryan@gmail.com>
2022-10-04 07:03:20 +0100

    Add basic user choice and bot choice functions

--- index.js
+++ index.js
@@ -4,20 +4,52 @@ const botScore = 0;
 
 const playerScore_span = document.getElementById("playerscore");
 const botScore_span = document.getElementById("botscore");
-
 const scoreBox_div = document.getElementById("score-box");
+const playerFace_div = document.getElementById("playerbox");
+const botFace_div = document.getElementById("botbox");
+const result_h3 = document.getElementById("result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
 
-
-const selectionButtons = document.querySelectorAll('[data-selection]')
-
-// Add event listener for each button - on 'click; listens for event ('e')
-selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e => {
-        const selectionName = selectionButton.dataset.selection
-        makeSelection(selectionName)
-    })
-})
-
-function makeSelection(selection) {
-    console.log(selection);
+function getBotChoice() {
+    const choices = ['r', 'p', 's'];
+    console.log(Math.floor(Math.random() * 3));
 }
+
+getBotChoice();
+
+function game(userChoice) {
+    
+}
+
+function main() {
+rock_div.addEventListener('click', function() {
+    game("r");
+})
+paper_div.addEventListener('click', function() {
+    game("p");
+})
+scissors_div.addEventListener('click', function() {
+    game("s");
+})
+}
+
+main();
+
+
+
+
+// const selectionButtons = document.querySelectorAll('[data-selection]')
+
+// // Add event listener for each button - on 'click; listens for event ('e')
+// selectionButtons.forEach(selectionButton => {
+//     selectionButton.addEventListener('click', e => {
+//         const selectionName = selectionButton.dataset.selection
+//         makeSelection(selectionName)
+//     })
+// })
+
+// function makeSelection(selection) {
+//     console.log(selection);
+// }
