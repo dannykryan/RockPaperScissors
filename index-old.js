32e81abf781010bf23750390118c2e62a30e7895
dannykryan <dannykryan@gmail.com>
2022-10-04 06:32:06 +0100

    Complete UI CSS Start new Javascript suitable for UI

---
+++ index-old.js
@@ -0,0 +1,59 @@
+let list = ["Rock", "Paper", "Scissors"];
+
+let playerLives = 5;
+let computerLives = 5;
+let round = 0;
+
+// this function generates a random number between 0-2 to select R/P/S from 'list' //
+function getComputerChoice () {
+	return list[Math.floor((Math.random()*list.length))];
+  }
+
+// this function compares the player selection to the computer selection and declares the player a winner or loser //
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
+	alert("You Win! Rock beats Scissors.");
+} else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
+	alert("You Lose! Paper beats Rock.");
+} else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Rock") {
+	alert("Round Draw!");
+} else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
+	alert("You Lose. Scissors beats Paper");
+} else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Paper") {
+	alert("Round Draw!");
+} else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
+	alert("You Win! Paper beats Rock.");
+} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Scissors") {
+	alert("Round Draw");
+} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
+	alert("You Win! Scissors beats Paper.");
+} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
+	alert("You Lose! Rock beats Scissors.");
+} else {
+    alert("try again");
+}
+}
+
+// loops the game round //
+for (let i = 0; i < 1; i++) {
+const playerSelection = prompt("Rock, Paper or Scissors", "Make a Choice");
+const computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
+}
+
+// target buttons DOM nodes and add event listener which changes playerSelection //
+
+const rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click', () => {
+  playerSelection("rock");
+});
+
+const paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click', () => {
+  playerSelection("paper");
+});
+
+const scissorsBtn = document.querySelector('.scissors');
+scissorsBtn.addEventListener('click', () => {
+  playerSelection("scissors");
+});
--- index.js
+++ index.js
@@ -1,67 +1,23 @@
-let list = ["Rock", "Paper", "Scissors"];
+// cacheing the DOM
+const playerScore = 0;
+const botScore = 0;
 
-// this function generates a random number between 0-2 to select R/P/S from 'list' //
-function getComputerChoice () {
-	return list[Math.floor((Math.random()*list.length))];
-  }
+const playerScore_span = document.getElementById("playerscore");
+const botScore_span = document.getElementById("botscore");
 
-// this function compares the player selection to the computer selection and declares the player a winner or loser //
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
-	alert("You Win! Rock beats Scissors.");
-} else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
-	alert("You Lose! Paper beats Rock.");
-} else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Rock") {
-	alert("Round Draw!");
-} else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
-	alert("You Lose. Scissors beats Paper");
-} else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Paper") {
-	alert("Round Draw!");
-} else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
-	alert("You Win! Paper beats Rock.");
-} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Scissors") {
-	alert("Round Draw");
-} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
-	alert("You Win! Scissors beats Paper.");
-} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
-	alert("You Lose! Rock beats Scissors.");
-} else {
-    alert("try again");
+const scoreBox_div = document.getElementById("score-box");
+
+
+const selectionButtons = document.querySelectorAll('[data-selection]')
+
+// Add event listener for each button - on 'click; listens for event ('e')
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        makeSelection(selectionName)
+    })
+})
+
+function makeSelection(selection) {
+    console.log(selection);
 }
-}
-
-// loops the game round //
-for (let i = 0; i < 1; i++) {
-const playerSelection = prompt("Rock, Paper or Scissors", "Make a Choice");
-const computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection);
-}
-
-// target buttons DOM nodes //
-// const rockButton = document.querySelector('.rock');
-// const paperButton = document.querySelector('.paper');
-// const scissorsButton = document.querySelector('.scissors');
-
-// const rockBtn = document.querySelector('.rock');
-// rockBtn.onclick = () => playerSelection = "rock";
-
-// const paperBtn = document.querySelector('.paper');
-// paperBtn.onclick = () => playerSelection = "paper";
-
-// const scissorsBtn = document.querySelector('.scissors');
-// scissorsBtn.onclick = () => playerSelection = "scissors";
-
-const rockBtn = document.querySelector('.rock');
-rockBtn.addEventListener('click', () => {
-  playerSelection("rock");
-});
-
-const paperBtn = document.querySelector('.paper');
-paperBtn.addEventListener('click', () => {
-  playerSelection("paper");
-});
-
-const scissorsBtn = document.querySelector('.scissors');
-scissorsBtn.addEventListener('click', () => {
-  console.log("scissors");
-});
